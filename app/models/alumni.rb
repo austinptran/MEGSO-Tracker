48d814b19f8de9ec26805d0c2225d3d7b5bcb6f8
Ashok Meyyappan <ashoksteelers12@gmail.com>
2022-02-13 15:09:53 -0600

    Alumni testing

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,2 +1,7 @@
 class Alumni < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :LinkedIn, presence: true
+    validates :alumni_occupation, presence: true
+    validates :alumni_grad_year, presence: true
 end
