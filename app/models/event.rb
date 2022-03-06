b62f156360b1429f2f89558f5350a713bd683908
shanebrown013@gmail.com <shanebrown013@gmail.com>
2022-03-06 17:20:38 -0600

    Deleted student_events files

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    has_many :attendee_lists
+    has_one :attendee_lists
 
     validates :event_name, :event_point, :event_location, :event_date, :event_start, :event_end, :event_verification, presence: true
 end
