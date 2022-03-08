c10c40f1ebcda823b0aeb481424410bfe03a5373
plantand000 <57603429+plantand000@users.noreply.github.com>
2022-03-07 23:25:55 -0600

    Navbar Implementation Version 1 (bootstrap needed)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,4 +7,9 @@ module UsersHelper
   def is_admin?(user)
     user.is_admin?
   end
+    # Returns the user's name
+  def get_current_user_first_name(uid)
+    @user = User.find_by(UID: uid)
+    @first_name = @user.first_name
+  end
 end
