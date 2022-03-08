8c204aab01e01238fcbd992f9ca8844ce3453e2a
shanebrown013@gmail.com <shanebrown013@gmail.com>
2022-03-07 18:15:06 -0600

    Removed redundant function

--- app/helpers/rewards_helper.rb
+++ app/helpers/rewards_helper.rb
@@ -1,9 +1,2 @@
 module RewardsHelper
-        # Returns the user's name
-      def get_current_user_points(uid)
-        @user = User.find_by(UID: uid)
-        @points = @user.points
-      end
-    
-
 end
