c13a6152921ace3fa47579fc42ee3cf098b8373a
justicenugo <justicenugo@gmail.com>
2022-03-22 10:47:53 -0500

    input verification for event & alumni pages

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,6 @@
 
 class Event < ApplicationRecord
   validates :event_name, :event_point, :event_location, :event_date, :event_start, :event_end, :event_verification, :event_attendee_list_id,
-            presence: true
+            presence: true, length: {minimum: 2, maximum: 50 }
+  
 end
