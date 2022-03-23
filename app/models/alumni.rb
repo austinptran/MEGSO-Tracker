b0d99c67563a41e1ae83b97e0026968ce9387c1c
justicenugo <justicenugo@gmail.com>
2022-03-23 06:57:17 -0500

    linkedin url validation
    incomplete

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -2,5 +2,6 @@
 
 class Alumni < ApplicationRecord
     validates :LinkedIn, :alumni_grad_year, :highest_position, presence: true, length: {minimum: 2, maximum: 50 }
-    validates_format_of :LinkedIn, :with => URI::regexp(%w(http https))
+    URL_REGEXP = /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix
+    validates :LinkedIn, format: { with: URL_REGEXP, message: 'You provided invalid LinkedIn URL' }
 end
