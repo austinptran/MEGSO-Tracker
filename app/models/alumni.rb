edd413e26678dce43a55dd67e66e42526011f042
justicenugo <justicenugo@gmail.com>
2022-03-23 05:48:16 -0500

    additional input validation
    input validation on rewards page

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class Alumni < ApplicationRecord
-validates :LinkedIn, :alumni_grad_year, :highest_position, presence: true, length: {minimum: 2, maximum: 50 }
+    validates :LinkedIn, :alumni_grad_year, :highest_position, presence: true, length: {minimum: 2, maximum: 50 }
 end
