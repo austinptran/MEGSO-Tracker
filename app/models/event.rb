a68c8c8be2f0f7b5517004ef4f8c0a759d89736c
justicenugo <justicenugo@gmail.com>
2022-04-09 08:27:05 -0500

    allows single digit points

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-  validates :event_name, :event_point, :event_location, :event_date, :event_start, :event_end, :event_verification, :event_attendee_list_id, :event_description,
+  validates :event_name, :event_location, :event_date, :event_start, :event_end, :event_verification, :event_attendee_list_id, :event_description,
             presence: true, length: {minimum: 2, maximum: 50 }
+  validates :event_point, presence: true
   #validates :event_verification, numericality: { only_integer: true }
   
 end
