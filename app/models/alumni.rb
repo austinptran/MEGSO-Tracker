59b9551080605c83894a1f7e68eb22a3d4eb9d45
austinptran <austinptran.2000@gmail.com>
2022-04-10 06:11:33 -0500

    Fixed issue where LinkedIn ID must be 3 characters; Brakeman should now fully work

--- app/models/alumni.rb
+++ app/models/alumni.rb
@@ -6,6 +6,6 @@ class Alumni < ApplicationRecord
   # URL_REGEXP = /\A(http|https):\/\/([\w]+\.]{1}linkedin+)*\.com{2,5}({1,5})?(\/.*)?\z/ix
   # URL_REGEXP = /https?:\/\/(?:\.be\/|(?:[a-z]{2,3}\.)?youtube\.com\/watch(?:\?|#\!)v=)([\w-]{11}).*/gi
   # URL_REGEXP = /\A(http\https):\/\/(www\.)?linkedin.com\/in\/(?P<permalink>[\w\-\_À-ÿ%]+)\/?
-  URL_REGEXP = %r{\Ahttps?://(www\.)?linkedin.com/in/([-_a-z0-9]{3})\z}
+  URL_REGEXP = %r{\Ahttps?://(www\.)?linkedin.com/in/[-_a-z0-9]{3,}\z}
   validates :LinkedIn, format: { with: URL_REGEXP, message: 'You provided invalid LinkedIn URL' }
 end
