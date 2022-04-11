861f073484791c2f3c48253a604f981d0e282a04
austinptran.2000 <austinptran.2000@gmail.com>
2022-04-10 20:11:10 -0500

    Reverting Commit

--- app/controllers/alumnis_controller.rb
+++ app/controllers/alumnis_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class AlumnisController < ApplicationController
-  before_action :authorize_user
   before_action :set_alumni, only: %i[show edit update destroy]
 
   # GET /alumnis or /alumnis.json
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    include SessionsHelper
-
-    helper_method :authorize_user
-
-    def authorize_user
-        redirect_to root_url unless logged_in?
-    end
+  include SessionsHelper
 end
