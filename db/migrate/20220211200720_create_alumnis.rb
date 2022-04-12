6ccc80c85339e762cdcdb3e0f47cd012dc5307ea
austinptran <austinptran.2000@gmail.com>
2022-04-11 20:17:26 -0500

    Fixed alumni name migrations

--- db/migrate/20220211200720_create_alumnis.rb
+++ db/migrate/20220211200720_create_alumnis.rb
@@ -1,7 +1,8 @@
 class CreateAlumnis < ActiveRecord::Migration[6.1]
   def change
     create_table :alumnis do |t|
-      t.string :alumni_name
+      t.string :alumni_first_name
+			t.string :alumni_last_name
       t.string :LinkedIn
       t.string :alumni_occupation
       t.integer :alumni_grad_year
